9ab567fdbdf0d8063173fd310d3eadc8aea56474
IsmailKoormath <ismailkoormath037@gmail.com>
2025-07-26 23:10:55 +0530

    update cors policy

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,25 +2,36 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import morgan from "morgan";
-
 import authRoutes from "./routes/auth.routes";
 import fileRoutes from "./routes/file.routes";
-import folderRoutes from "./routes/folder.routes";
+import folderRoutes from "./routes/folder.routes"
 
 const app = express();
 
-const corsOptions = {
-  origin: process.env.CLIENT_URL || "http://localhost:3000",
-  credentials: true,
-};
+// Middlewares
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://secure-file-storage-system-m757.vercel.app",
+];
 
-app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
-
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+  
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
 
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/files", fileRoutes);
 app.use("/api/folders", folderRoutes);
