9d93427d739cfb38138e864f64fcaad7f6e3cc24
IsmailKoormath <ismailkoormath037@gmail.com>
2025-07-26 22:43:37 +0530

    update the cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,24 +2,25 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import morgan from "morgan";
+
 import authRoutes from "./routes/auth.routes";
 import fileRoutes from "./routes/file.routes";
-import folderRoutes from "./routes/folder.routes"
+import folderRoutes from "./routes/folder.routes";
 
 const app = express();
 
-// Middlewares
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
-    credentials: true,
-  })
-);
+const corsOptions = {
+  origin: process.env.CLIENT_URL || "http://localhost:3000",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
 
-// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/files", fileRoutes);
 app.use("/api/folders", folderRoutes);
